eb9c4fa110169b4f42e99affe54e74c3018b50be
Ratish Panda <ratishp2001@gmail.com>
2025-04-08 20:41:59 +0530

    add gemini 2.5 pro preview model token limit

--- pr_agent/algo/__init__.py
+++ pr_agent/algo/__init__.py
@@ -53,6 +53,7 @@ MAX_TOKENS = {
     'gemini/gemini-1.5-pro': 1048576,
     'gemini/gemini-1.5-flash': 1048576,
     'gemini/gemini-2.0-flash': 1048576,
+    'gemini/gemini-2.5-pro-preview-03-25': 1048576,
     'codechat-bison': 6144,
     'codechat-bison-32k': 32000,
     'anthropic.claude-instant-v1': 100000,
